5b9866e4e19ec8041afcd515bc98dcecfe3e4d48
NaellyV <naellyvitoria8@gmail.com>
2025-03-08 11:12:48 -0300

    adcionando imagem da home no deploy

--- src/image/page.tsx
+++ src/image/page.tsx
@@ -2,8 +2,7 @@
 
 
 import Image from "next/image";
-import inicio from "./inicio.svg";
-
+import inicio from "../../public/inicio.svg"
 export default function Inicio() {
   return (
     <Image src={inicio} width={500} height={500} alt="Picture of the author" />
